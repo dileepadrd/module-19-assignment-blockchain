bc55db0beb1624fb47d2e5a1770f46509c98486c
dileepa <dileeparanjan@gmail.com>
2023-04-13 15:44:22 -0400

    Getting the account via generate_account function

--- krypto_jobs.py
+++ krypto_jobs.py
@@ -156,7 +156,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = generate_account()
 
 ##########################################
 
